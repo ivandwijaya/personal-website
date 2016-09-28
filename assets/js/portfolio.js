413e28e5219015994636328528beb8b408291d0c
Ivan Dwijaya <flc.re1t24@gmail.com>
2016-09-28 18:58:38 +0700

    Add files via upload

---
+++ assets/js/portfolio.js
@@ -0,0 +1,48 @@
+var apiKey = 'NbsCl3bntz4BuK8mf2wiJ8VoNGGDyswk';
+var userID = 'ivandwijaya';
+        
+//Javascript Fetch User Data
+/*(function() {
+    var behanceUserAPI = 'http://www.behance.net/v2/users/'+ userID +'?callback=?&api_key='+ apiKey;
+    function setUserTemplate() {
+        var userData    = JSON.parse(sessionStorage.getItem('behanceUser')),
+        getTemplate = $('#profile-template').html(),
+        template    = Handlebars.compile(getTemplate),
+        result      = template(userData);
+        $('#header').html(result);
+    };
+    if(sessionStorage.getItem('behanceUser')) {
+        setUserTemplate();
+    } else {
+        $.getJSON(behanceUserAPI, function(user) {
+            var data = JSON.stringify(user);
+            sessionStorage.setItem('behanceUser', data);
+            setUserTemplate();
+        });
+    };
+})();*/
+    
+
+//Javascript Fetch Projects
+(function() {
+    var perPage = 12;
+    var behanceProjectAPI = 'http://www.behance.net/v2/users/'+ userID +'/projects?callback=?&api_key=' + apiKey + '&per_page=' + perPage;
+
+    function setPortfolioTemplate() {
+        var projectData = JSON.parse(sessionStorage.getItem('behanceProject')),
+            getTemplate = $('#portfolio-template').html(),
+            template    = Handlebars.compile(getTemplate),
+            result      = template(projectData);
+        $('#portfolio').html(result);
+    };
+
+    if(sessionStorage.getItem('behanceProject')) {
+        setPortfolioTemplate();
+    } else {
+        $.getJSON(behanceProjectAPI, function(project) {
+            var data = JSON.stringify(project);
+            sessionStorage.setItem('behanceProject', data);
+            setPortfolioTemplate();
+        });
+    };
+})();
